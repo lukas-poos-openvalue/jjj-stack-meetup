3b54e543c69b3f53bfaadcf4a2ef68861195acef
Lukas Poos <lukas.poos@openvalue.de>
2024-05-25 14:39:35 +0200

    Update Javalin to 6.0.X

--- src/main/java/de/lpo/todo/TodoAppPlusMain.java
+++ src/main/java/de/lpo/todo/TodoAppPlusMain.java
@@ -16,60 +16,66 @@ public class TodoAppPlusMain {
         final var profile = APP_CTX.configService.getProfile();
         final var config = APP_CTX.configService.getConfig();
 
-        // Initialize application
-        final var app = Javalin.create(appConfig -> appConfig.staticFiles.add(staticFiles -> {
-            // [PoI] Based on profile, serve assets from files or from resources
-            final boolean isDev = profile == Profile.DEV;
-            staticFiles.hostedPath = "/";
-            staticFiles.location = isDev ? Location.EXTERNAL : Location.CLASSPATH;
-            staticFiles.directory = isDev ? "src/main/resources/assets" : "/assets";
-            staticFiles.precompress = !isDev;
-        }));
+        final var app = Javalin.create(appConfig -> {
+            // General config
+            appConfig.useVirtualThreads = true;
+            appConfig.http.brotliAndGzipCompression();
 
-        // [PoI] Setup routes
-        app.routes(() -> {
-            get(APP_CTX.indexPageHandler);
-
-            path("api", () -> {
-                get("login", ctx -> ctx.redirect("/app"));
-                get("login/callback", APP_CTX.oidcSecurityService.handleOidcLoginCallback("/app"));
-                get("logout", APP_CTX.oidcSecurityService.handleOidcLogout("/"));
+            // Serve assets as static files
+            appConfig.staticFiles.add(staticFiles -> {
+                // [PoI] Based on profile, serve assets from files or from resources
+                final boolean isDev = profile == Profile.DEV;
+                staticFiles.hostedPath = "/";
+                staticFiles.location = isDev ? Location.EXTERNAL : Location.CLASSPATH;
+                staticFiles.directory = isDev ? "src/main/resources/assets" : "/assets";
+                staticFiles.precompress = !isDev;
             });
 
-            get("app", ctx -> ctx.redirect("/app/boards"));
-            path("app", () -> {
-                before(APP_CTX.oidcSecurityService.handleOidcSecurity()); // [PoI] Everything below uses OIDC security
+            // [PoI] Setup routes
+            appConfig.router.apiBuilder(() -> {
+                get(APP_CTX.indexPageHandler);
 
-                before("admin", APP_CTX.oidcSecurityService.handleAdminRoleRequired()); // [PoI] Admin path uses role security
-                get("admin", APP_CTX.adminPageHandler);
+                path("api", () -> {
+                    get("login", ctx -> ctx.redirect("/app"));
+                    get("login/callback", APP_CTX.oidcSecurityService.handleOidcLoginCallback("/app"));
+                    get("logout", APP_CTX.oidcSecurityService.handleOidcLogout("/"));
+                });
 
-                path("boards", () -> {
-                    get(APP_CTX.boardsPageHandler);
-                    post(APP_CTX.boardsAddActionHandler);
+                get("app", ctx -> ctx.redirect("/app/boards"));
+                path("app", () -> {
+                    before(APP_CTX.oidcSecurityService.handleOidcSecurity()); // [PoI] Everything below uses OIDC security
 
-                    path("{board-id-hashed}", () -> {
-                        before(APP_CTX.boardSecurityService.handleBoardAccess()); // [PoI] Board path uses access security
-                        patch(APP_CTX.boardEditActionHandler);
-                        delete(APP_CTX.boardDeleteActionHandler);
+                    before("admin", APP_CTX.oidcSecurityService.handleAdminRoleRequired()); // [PoI] Admin path uses role security
+                    get("admin", APP_CTX.adminPageHandler);
 
-                        path("todos", () -> {
-                            get(APP_CTX.todosPageHandler);
-                            post(APP_CTX.todosAddActionHandler);
+                    path("boards", () -> {
+                        get(APP_CTX.boardsPageHandler);
+                        post(APP_CTX.boardsAddActionHandler);
 
-                            path("{todo-id-hashed}", () -> {
-                                get(APP_CTX.todoItemHandler);
-                                patch(APP_CTX.todoEditActionHandler);
-                                delete(APP_CTX.todoDeleteActionHandler);
+                        path("{board-id-hashed}", () -> {
+                            before(APP_CTX.boardSecurityService.handleBoardAccess()); // [PoI] Board path uses access security
+                            patch(APP_CTX.boardEditActionHandler);
+                            delete(APP_CTX.boardDeleteActionHandler);
+
+                            path("todos", () -> {
+                                get(APP_CTX.todosPageHandler);
+                                post(APP_CTX.todosAddActionHandler);
+
+                                path("{todo-id-hashed}", () -> {
+                                    get(APP_CTX.todoItemHandler);
+                                    patch(APP_CTX.todoEditActionHandler);
+                                    delete(APP_CTX.todoDeleteActionHandler);
+                                });
                             });
                         });
                     });
-                });
 
-                path("modals", () -> {
-                    path("/boards/add", () -> get(APP_CTX.boardsAddModalHandler));
-                    path("/boards/{board-id-hashed}/edit", () -> {
-                        before(APP_CTX.boardSecurityService.handleBoardAccess()); // [PoI] Board path uses access security
-                        get(APP_CTX.boardEditModalHandler);
+                    path("modals", () -> {
+                        path("/boards/add", () -> get(APP_CTX.boardsAddModalHandler));
+                        path("/boards/{board-id-hashed}/edit", () -> {
+                            before(APP_CTX.boardSecurityService.handleBoardAccess()); // [PoI] Board path uses access security
+                            get(APP_CTX.boardEditModalHandler);
+                        });
                     });
                 });
             });
